1e76c0e77514de4a342f333d2fba6c171757f06f
Emma Demarecaux <emmademarecaux@MacBook-Pro-de-Emma.local>
2020-01-31 19:05:14 +0100

    Updates

--- problem.py
+++ problem.py
@@ -5,6 +5,7 @@ import rampwf as rw
 from rampwf.workflows import FeatureExtractorRegressor
 from rampwf.score_types.base import BaseScoreType
 from sklearn.model_selection import GroupShuffleSplit
+from sklearn.metrics import f1_score
 
 
 problem_title = 'Prediction of inspection score'
@@ -42,9 +43,28 @@ class Penalised_RMSE(BaseScoreType):
         loss = np.sqrt(np.mean((y_true - y_pred)**2*penalization))
         return loss
 
+# F1-Score on Categories B + C
+class f1score_category(BaseScoreType):
+    is_lower_the_better = False
+    minimum = 0.0
+    maximum = 1.0
+
+    def __init__(self, name='f1score', precision=2):
+        self.name = name
+        self.precision = precision
+
+    def __call__(self, y_true, y_pred):
+        y_pred_binary = np.vectorize(lambda x : 0 if (x >= 90) else 1)(y_pred)
+        y_true_binary = np.vectorize(lambda x : 0 if (x >= 90) else 1)(y_true)
+        score = f1_score(y_true_binary, y_pred_binary, average='weighted')
+        return score
+
+
 score_types = [
     # Penalised root mean square error
     Penalised_RMSE(name='Score error', precision=2),
+    # F1-Score on Categories B+C
+    f1score_category(name='F1 score', precision=2)
 ]
 
 #--------------------------------------------
